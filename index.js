86c69a37f8ed3fec6c8f41ca4446419de624f15b
Benard Oneka <benardoneka@gmail.com>
2023-01-10 21:39:34 +0300

    first app

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Benard Oneka');
+
 });
 
 server.listen(port, hostname, () => {
